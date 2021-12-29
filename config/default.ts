c1a0e51cc9132cc4e5fb2ef6f6a6322bcd7caa47
zrwusa <zrwusa@gmail.com>
2021-12-29 17:14:29 +0800

    rename the db connection to bunny-rest

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
     PORT: 8080,
-    DB_URI: 'mongodb://localhost:27017/bunny-server-ts',
+    DB_URI: 'mongodb://localhost:27017/bunny-rest',
     SALT_WORK_FACTOR: 10,
     ACCESS_TOKEN_TTL: '0.5m',
     REFRESH_TOKEN_TTL: '1y',
