66d13db5ecb30f40562f82bdd76033c63052b83f
zrwusa <zrwusa@gmail.com>
2022-01-03 20:47:04 +0800

    docker swarm

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ git pull
 
 echo "Building application"
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+#docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
+#docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --no-deps bunny-rest
